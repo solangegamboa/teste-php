d00ef9f79f2bcdb3048c42bc9e1658f203ed2e98
Solange Gamboa <solange.gamboa@gmail.com>
2015-12-24 08:58:13 -0200

    correção font-awesome

--- questao4/app/views/index.php
+++ questao4/app/views/index.php
@@ -7,7 +7,7 @@
 
     <!-- CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
 
 </head>
